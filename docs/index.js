230b85636d3d9b6699a721c6992e1e20e7d12a39
Muneet <muneetj@gmail.com>
2020-04-20 23:40:22 +0100

    initial upload

---
+++ docs/index.js
@@ -0,0 +1,23 @@
+function navClick(stack,menu){
+    
+    stack.classList.toggle("active");
+    var menu = document.getElementById("links")
+    if(menu.style.display === "flex"){
+        menu.style.display = "none";
+    }else{
+        menu.style.display = "flex";
+    }
+}
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("navBar").style.height = "3.5vh"
+    document.getElementById("navBar").style.fontSize = "1em"
+  } else {
+    document.getElementById("navBar").style.height = "10vh"
+    document.getElementById("navBar").style.fontSize = "1.5em"
+  }
+}
+
