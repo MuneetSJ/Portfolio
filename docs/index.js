5c1e7d565a340b3148d5d3438da6594350b9591e
Muneet <muneetj@gmail.com>
2020-05-14 21:04:20 +0100

    Design update

--- docs/index.js
+++ docs/index.js
@@ -12,12 +12,15 @@ function navClick(stack,menu){
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("navBar").style.height = "3.5vh"
-    document.getElementById("navBar").style.fontSize = "1em"
-  } else {
-    document.getElementById("navBar").style.height = "10vh"
-    document.getElementById("navBar").style.fontSize = "1.5em"
+  if(!window.matchMedia("(max-width: 600px)").matches){
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      document.getElementById("navBar").style.height = "3.5vh"
+      document.getElementById("navBar").style.fontSize = "1em"
+    } else {
+      document.getElementById("navBar").style.height = "10vh"
+      document.getElementById("navBar").style.fontSize = "1.5em"
+    }
   }
+  
 }
 
