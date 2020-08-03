edb68a5ca235e0425f84590d289211f3eaa720ed
Muneet Jaswal <Muneetj@gmail.com>
2020-08-03 23:56:44 +0100

    Update V2
    New Design, not opitmal for mobile or tablets, but responsice

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,30 @@
-function navClick(stack,menu){
+var x = 0;
+var y = 0;
+var header = "Hello World!";
+var para = "I am Muneet Jaswal. A Computer Science and AI student at the University of Sussex."
+var media = window.matchMedia("(max-width: 600px)")
+
+window.onscroll = function() {scrollFunction()};
+window.onload = function headingText(){
+    if(x < header.length){
+      var textbox = document.getElementById("welcome-text-heading");
+      textbox.innerHTML += header.charAt(x);
+      x++;
+      setTimeout(headingText,80);
+    }
+    setTimeout(paraText,1000)
+}
+
+function paraText(){
+  if(y < para.length){
+    var textbox = document.getElementById("welcome-text-paragraph");
+    textbox.innerHTML += para.charAt(y);
+    y++;
+    setTimeout(paraText,500);
+  }
+}
+
+function navClick(stack){
     
     stack.classList.toggle("active");
     var menu = document.getElementById("links")
@@ -9,7 +35,12 @@ function navClick(stack,menu){
     }
 }
 
-window.onscroll = function() {scrollFunction()};
+function hideMenu(){
+  if(media.matches){
+    var stack = document.getElementById("stack");
+    stack.classList.toggle("active")
+  }
+}
 
 function scrollFunction() {
   if(!window.matchMedia("(max-width: 600px)").matches){
@@ -21,6 +52,6 @@ function scrollFunction() {
       document.getElementById("navBar").style.fontSize = "1.5em"
     }
   }
-  
 }
+
 
